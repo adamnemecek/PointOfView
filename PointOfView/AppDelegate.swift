f0a4fe7086e1c8d2c5ce9b628eed78c25a1a3d7f
Lucas Myers <lcmylin@gmail.com>
2018-03-30 00:03:55 -0500

    Initial Commit

---
+++ PointOfView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PointOfView
+//
+//  Created by Lucas Myers on 3/30/18.
+//  Copyright © 2018 Myang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PointOfView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PointOfView
+//
+//  Created by Lucas Myers on 3/30/18.
+//  Copyright © 2018 Myang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

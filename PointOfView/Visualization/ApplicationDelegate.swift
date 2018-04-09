91e9ad9520f5b1144bfa9740565101c59b27fa29
Lucas Myers <lcmylin@gmail.com>
2018-04-09 16:21:11 -0500

    Load the first point cloud on launch

--- PointOfView/Visualization/ApplicationDelegate.swift
+++ PointOfView/Visualization/ApplicationDelegate.swift
@@ -2,6 +2,17 @@ import Cocoa
 
 @NSApplicationMain
 public class ApplicationDelegate: NSObject, NSApplicationDelegate {
+    var pointCloud: PointCloud! = nil
+    
+    public func applicationWillFinishLaunching(_ notification: Notification) {
+        pointCloud = try! PointCloud(contentsOf: Bundle.main.url(forResource: "pointcloud1", withExtension: "fuse")!)
+    }
+    
+    public func applicationDidFinishLaunching(_ notification: Notification) {
+        guard let viewController = NSApp.mainWindow?.contentViewController as? ViewController else { return }
+        viewController.pointCloud = pointCloud
+    }
+    
     public func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
